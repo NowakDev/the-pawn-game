e19c44a6f6e56288dad4e5b673ad2e09f67d350d
Kamil Nowak <it.kamilnowak@gmail.com>
2019-12-17 17:55:38 +0100

    favicon and player select container added (pawns selection)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import PlayerSelect from './containers/PlayerSelect'
+import './App.css'
 
-function App() {
+const App = (props) => {
   return (
-    <PlayerSelect />
+    <div className="App">
+      <PlayerSelect />
+    </div>
   )
 }
 
--- src/containers/PlayerSelect.js
+++ src/containers/PlayerSelect.js
@@ -3,20 +3,13 @@ import React from 'react'
 const styles = {
   pawnBox: {
     display: 'flex',
+    justifyContent: 'center'
   },
-  bluePawn: {
+  pawn: {
     width: 100,
     height: 100,
-    margin: 10,
+    margin: 20,
     cursor: 'pointer',
-    backgroundColor: 'blue'
-  },
-  redPawn: {
-    width: 100,
-    height: 100,
-    margin: 10,
-    cursor: 'pointer',
-    backgroundColor: 'red'
   }
 }
 
@@ -43,25 +36,33 @@ class PlayerSelect extends React.Component {
   }
 
   render() {
-    console.log(this.state)
+    const { pawns, currentPlayer, players } = this.state
     return (
-      <div>
-        <h1>Player {this.state.currentPlayer}, select a pawn:</h1>
-        <div style={styles.pawnBox}>
-          <div
-            style={styles.bluePawn}
-            onClick={() => this.pawnSelectHandler('blue')}
-          >
-            BLUE
-            </div>
-          <div
-            style={styles.redPawn}
-            onClick={() => this.pawnSelectHandler('red')}
-          >
-            RED
-            </div>
+      players.length === 0 ?
+        <div>
+          <h1>Player {currentPlayer}, select a pawn:</h1>
+          <div style={styles.pawnBox}>
+            {
+              pawns.map(pawn => (
+                <div
+                  key={pawn}
+                  style={styles.pawn}
+                  onClick={() => this.pawnSelectHandler(`${pawn}`)}
+                >
+                  <img
+                    src={`./pawns/${pawn}-pawn.png`}
+                    alt={`${pawn}-pawn.png`}
+                  />
+                </div>
+              ))
+            }
+          </div>
         </div>
-      </div>
+        :
+        <div>
+          <h1>Player 1: {players[0].pawnColor}</h1>
+          <h1>Player 2: {players[1].pawnColor}</h1>
+        </div>
     )
   }
 }
