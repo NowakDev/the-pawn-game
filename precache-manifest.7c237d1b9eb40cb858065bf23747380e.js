90fa64f8616c9a8e88c692204adbc50e930c3a59
Kamil Nowak <it.kamilnowak@gmail.com>
2019-12-24 01:04:48 +0100

    Updates

---
+++ precache-manifest.7c237d1b9eb40cb858065bf23747380e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550bac917c5a4085efeeb33f8785b6c8",
+    "url": "/the-pawn-game/index.html"
+  },
+  {
+    "revision": "48218e0e2865a054a8c8",
+    "url": "/the-pawn-game/static/css/main.ce594100.chunk.css"
+  },
+  {
+    "revision": "b422b9fc90a83a492082",
+    "url": "/the-pawn-game/static/js/2.88d9ad1b.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/the-pawn-game/static/js/2.88d9ad1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48218e0e2865a054a8c8",
+    "url": "/the-pawn-game/static/js/main.4487c3b8.chunk.js"
+  },
+  {
+    "revision": "4959ff29d903a55dab87",
+    "url": "/the-pawn-game/static/js/runtime-main.0eb35121.js"
+  }
+]);
